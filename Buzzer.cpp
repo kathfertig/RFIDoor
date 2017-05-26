4a9aa0fdab0c3bde6463b9e297f801e5138a0178
kathfertig <kathfertig@gmail.com>
2017-05-26 18:51:36 -0300

    Criação de classe de buzzer.

---
+++ Buzzer.cpp
@@ -0,0 +1,60 @@
+/*
+ * Buzzer.cpp
+ *
+ *  Created on: 26 de mai de 2017
+ *      Author: maedel
+ */
+
+#include "Buzzer.h"
+
+
+Buzzer::Buzzer(int pin, Buzzer_t bz) : _buzzer_config(pin, GPIO::OUTPUT) {
+
+	switch(bz){
+	case _buzzer0:
+		//configuração 0:
+		_frequencia = 0;
+		_tempo = 0;
+		break;
+
+	case _buzzer1:
+		//configuração 1: 1 bip longo alto
+		//_frequencia = 0;
+		//_tempo = 0;
+		break;
+
+	case _buzzer2:
+		//configuração 2: 1 bip longo baixo
+		//_frequencia = 0;
+		//_tempo = 0;
+	break;
+
+	case _buzzer3:
+		//configuração 3: 3 bips curtos altos
+		//_frequencia = 0;
+		//_tempo = 0;
+		break;
+
+	case _buzzer4:
+		//configuração 4: 3 bips curtos baixos
+		//_frequencia = 0;
+		//_tempo = 0;
+	break;
+	}
+}
+
+Buzzer::~Buzzer(){}
+
+void Buzzer::aciona_config(){
+	Hertz val = (500000/(_frequencia));
+	//long int t = _miliseconds*1000 / (1000/_frequencia);
+	Hertz t = (_tempo*50) / (val*2);
+
+	for(long int i=0; i < t; i++){
+		_buzzer_config.set(true);
+		timer_aciona.udelay(val);
+		_buzzer_config.set(false);
+		timer_aciona.udelay(val);
+	}
+}
+
---
+++ Buzzer.h
@@ -0,0 +1,34 @@
+/*
+ * Buzzer.h
+ *
+ *  Created on: 26 de mai de 2017
+ *      Author: maedel
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include "GPIO.h"
+#include "Timer.h"
+
+Timer timer_aciona(1000);
+typedef long int Hertz;
+
+class Buzzer {
+public:
+
+	enum Buzzer_t{_buzzer0 = 0, _buzzer1 = 1, _buzzer2 = 2, _buzzer3 = 3, _buzzer4 = 4};
+
+	Buzzer(int pin, Buzzer_t = _buzzer0);
+	~Buzzer();
+
+	//Buzzer(int pin,int freq, int miliseconds);
+	void aciona_config();
+
+private:
+	GPIO _buzzer_config;
+	Hertz _frequencia;
+	Hertz _tempo;
+};
+
+#endif /* BUZZER_H_ */
