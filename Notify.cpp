81c0b40bbbc54d226dbd8ae95aea829750b39e0e
kathfertig <kathfertig@gmail.com>
2017-05-30 18:18:21 -0300

    Adição de classe notificadora. Falta implementar os metodos.

---
+++ Notify.cpp
@@ -0,0 +1,10 @@
+/*
+ * Notify.cpp
+ *
+ *  Created on: 30 de mai de 2017
+ *      Author: maedel
+ */
+
+#include "Notify.h"
+
+
---
+++ Notify.h
@@ -0,0 +1,37 @@
+/*
+ * Notify.h
+ *
+ *  Created on: 30 de mai de 2017
+ *      Author: maedel
+ */
+
+#ifndef NOTIFY_H_
+#define NOTIFY_H_
+
+//#include "Timer.h"
+//Timer pausa(1000);
+
+
+class Notify {
+public:
+	Notify();
+	~Notify();
+
+		void notify_acesso(){
+
+
+		}
+		void notify_sucesso(){
+
+		}
+
+		//void notify_warning();
+
+
+private:
+
+
+
+};
+
+#endif /* NOTIFY_H_ */
